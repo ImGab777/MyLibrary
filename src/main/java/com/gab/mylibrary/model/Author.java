e313d4aa03fe10034c785394ea308e9423933466
Gabriel <gabeefesouzask8@gmail.com>
2025-06-12 15:12:21 -0300

    entitie Author

--- src/main/java/com/gab/mylibrary/model/Author.java
+++ src/main/java/com/gab/mylibrary/model/Author.java
@@ -1,4 +1,58 @@
 package com.gab.mylibrary.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = true)
+    private LocalDate deathDate;
+
+    // Relacionamento One-to-Many com Livro
+    // mappedBy indica qual campo na entidade 'Book' é o "dono" do relacionamento
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
+
+    public Author(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Author(String name, LocalDate birthDate, LocalDate deathDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+    }
+
+    public void addBook(Book book){
+        this.books.add(book);
+        book.setAuthor(this);
+    }
+
+    public void removeBook(Book book){
+        this.books.remove(book);
+        book.setAuthor(null);
+    }
 }
--- src/main/java/com/gab/mylibrary/model/Book.java
+++ src/main/java/com/gab/mylibrary/model/Book.java
@@ -2,10 +2,10 @@ package com.gab.mylibrary.model;
 
 import com.gab.mylibrary.model.enuns.Genre;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Entity
 @Table(name = "books")
@@ -18,19 +18,35 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Genre genre;
 
-    private Integer publicationYear;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 
-    @Column(unique = true)
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    private LocalDate publicationYear;
+
+    @Column(unique = true, nullable = false)
     private String isbn;
 
     @ManyToOne
     @JoinColumn(name = "author_id", nullable = false)
     private Author author;
 
+    public Book(String title, Genre genre, String longDescription, String shortDescription, LocalDate publicationYear, String isbn, Author author) {
+        this.title = title;
+        this.genre = genre;
+        this.longDescription = longDescription;
+        this.shortDescription = shortDescription;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.author = author;
+    }
 }
